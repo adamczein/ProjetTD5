8233e580b7270aaaacd731baa4a4a7eeb20d6606
adamczein <112838729+adamczein@users.noreply.github.com>
2023-06-07 21:00:14 -0400

    Add project files.

---
+++ Noeud.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <gsl/pointers>
+template<typename T> class ListeLiee;
+template<typename T> class Iterateur;
+template<typename T>
+class Noeud
+{
+public:
+	//TODO: Constructeur(s).
+private:
+	//TODO: Attributs d'un noeud.
+	friend class ListeLiee<T>;
+	friend class Iterateur<T>;
+};
